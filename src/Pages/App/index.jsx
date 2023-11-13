4fab99dd581b3e0d6057476de492e7f719c4f6b5
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-13 10:03:01 -0300

    :sparkles: Navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import MyOrder from "../MyOrder";
 import MyOrders from "../MyOrders";
 import NotFound from "../NotFound";
 import SignIn from "../SignIn";
+import Navbar from "../../Components/Navbar";
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -40,6 +41,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Navbar />
     </BrowserRouter>
   );
 };
