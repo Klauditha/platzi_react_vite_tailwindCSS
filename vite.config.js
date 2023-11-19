55aff05fa26caa193d2f730dbc36097c2a881e1c
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-18 23:55:07 -0300

    :zap: Deploy 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://klauditha.github.io/platzi_react_vite_tailwindCSS",
+  //base:"https://klauditha.github.io/platzi_react_vite_tailwindCSS",
   plugins: [react()],
 })
